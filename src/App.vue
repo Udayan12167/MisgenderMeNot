a8ff28c2070f12f94b214fa327d22f78c80a1cad
Udayan Tandon <utandon@eng.ucsd.edu>
2020-11-13 14:26:35 -0800

    HelloWorld: Change component name to StartScreen

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Misgender Me Not"/>
+    <StartScreen msg="Misgender Me Not"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import StartScreen from './components/StartScreen.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    StartScreen
   }
 }
 </script>
