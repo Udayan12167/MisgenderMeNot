3479281c38d87baf42428e98e5455d12f01f8ec1
Udayan Tandon <utandon@eng.ucsd.edu>
2020-11-13 15:00:34 -0800

    main: Add vue router

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
 Vue.use(VueMaterial)
+Vue.use(VueRouter)
 
 new Vue({
   render: h => h(App),
