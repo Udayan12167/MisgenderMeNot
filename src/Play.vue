a58752cf6d591da50ee0e5c96e31af357b485776
Udayan Tandon <utandon@eng.ucsd.edu>
2020-11-15 14:46:51 -0800

    Play: Add play view with menubar

---
+++ src/Play.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="play">
+        <md-app md-mode="reveal">
+            <MenuBar title="Game 1"/>
+        </md-app>
+    </div>
+</template>
+
+<script>
+import MenuBar from './components/MenuBar.vue'
+
+export default {
+    name: 'Play',
+    components: {
+        MenuBar
+    }
+}
+</script>
