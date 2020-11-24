54fe388e92e023ef9b5815614f5df0114fc35a3d
Mary Anne Smart <msmart@eng.ucsd.edu>
2020-11-24 11:51:26 -0800

    filled out bare bones of resource page, formatting still to do

--- src/components/Resources.vue
+++ src/components/Resources.vue
@@ -1,5 +1,14 @@
 <template>
-<p>To Do</p>
+  <div>
+    <h1>Resources</h1>
+    <div id='resource-wrapper'>
+      <p class='resource-item'> <a href="https://www.cdc.gov/lgbthealth/youth-resources.htm"> CDC LGBT Youth Resource </a> </p>
+      <p class='resource-item'> <a href="https://en.wikipedia.org/wiki/Gender_neutrality_in_languages_with_gendered_third-person_pronouns"> Gender neutrality in languages | Wikipedia Page </a> </p>
+      <p class='resource-item'> <a href="https://www.pewresearch.org/fact-tank/2019/09/05/gender-neutral-pronouns/"> Pew Research Center Study on Pronoun Popularity </a> </p>
+      <p class='resource-item'> <a href="https://time.com/4327915/gender-neutral-pronouns/"> TIME: Everything You Ever Wanted to Know About Pronouns </a> </p>
+      <p class='resource-item'> <a href="https://youtu.be/gXLFdYNEl_I"> Gender Pronouns, Get Them Right! | MTV Life Video </a> </p>
+    </div>
+  </div>
 </template>
 
 <script>
