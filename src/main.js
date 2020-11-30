657567f3ddf4b968b67d2d3957e236e15b326673
Mary Anne Smart <msmart@eng.ucsd.edu>
2020-11-30 11:00:13 -0800

    attempting to fix merge conflicts

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import Play from './components/Play.vue'
 import StartScreen from './components/StartScreen.vue'
 import Resources from './components/Resources.vue'
 import NotFound from './components/NotFound.vue'
+import Select from './components/Select.vue'
+// import NotFound from './NotFound.vue'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
@@ -21,6 +23,8 @@ const router = new VueRouter({
       children: [
         { path: '', component: StartScreen },
         { path: 'play', component: Play },
+        { path: 'game1', component: Select},
+        { path: 'game1/sentence', component: SentenceCorrect}
         {path: 'resources', component: Resources},
         { path: "*", component: NotFound }
       ]
