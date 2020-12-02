c654786b22d8d6a7d8fbf8e878f31657d89bedf5
Udayan Tandon <utandon@eng.ucsd.edu>
2020-12-02 12:16:33 -0800

    main: Add persistent storage

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ import './assets/theme.scss'
 import VueRouter from 'vue-router'
 import VueConfetti from 'vue-confetti'
 import Vuex from 'vuex'
+import createPersistedState from "vuex-persistedstate";
 import SENTENCES from './resources/sentences_game_1.json'
 
 Vue.config.productionTip = false
@@ -74,7 +75,8 @@ const store = new Vuex.Store({
           return false;
       });
     }
-  }
+  },
+  plugins: [createPersistedState()]
 });
 
 Vue.mixin({
